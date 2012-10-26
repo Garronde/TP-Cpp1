38e641cb70d6ccf24697af8c752117a1d6fafda7
garronde <garrondeau@gmail.com>
2012-10-26 14:39:07 +0200

    création de la classe coup

---
+++ coup.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   coup.cpp
+ * Author: garrondeau
+ * 
+ * Created on 26 octobre 2012, 14:38
+ */
+
+#include "coup.h"
+
+coup::coup() {
+}
+
+coup::coup(const coup& orig) {
+}
+
+coup::~coup() {
+}
+
---
+++ coup.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   coup.h
+ * Author: garrondeau
+ *
+ * Created on 26 octobre 2012, 14:38
+ */
+
+#ifndef COUP_H
+#define	COUP_H
+
+class coup {
+public:
+    coup();
+    coup(const coup& orig);
+    virtual ~coup();
+private:
+
+};
+
+#endif	/* COUP_H */
+
