59adc38bc768e01fb85b17392ff2a70575c848c6
Tony Martin <tony.martin313@gmail.com>
2012-10-26 14:29:41 +0200

    Démarrage projet

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Tony Martin
+ *
+ * Created on 26 octobre 2012, 14:24
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+        return 0;
+}
+
